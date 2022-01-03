8789b3d1729a35d110b0c868b165b7fa3af6a9c8
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2022-01-02 20:22:48 -0500

    Create appointment.swift

---
+++ Libra/View/appointment.swift
@@ -0,0 +1,14 @@
+//
+//  appointment.swift
+//  LibraFinalVersion
+//
+//  Created by Vivaan Baid on 24/10/21.
+//
+
+import Foundation
+
+struct appointment{
+    var full_name: String
+    var date: String
+    var typeOfTherapist: String
+}
