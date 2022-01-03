2a57657457f35439ecfdcbd3d779895bdb2c19e3
Meliora Ho <85920674+mellieho9@users.noreply.github.com>
2022-01-02 20:22:04 -0500

    Add files via upload

---
+++ Libra/K.swift
@@ -0,0 +1,15 @@
+//
+//  K.swift
+//  LibraFinalVersion
+//
+//  Created by Vivaan Baid on 23/10/21.
+//
+
+import Foundation
+struct K{
+    
+    //holds all the constants
+    static let IntroToIntialcheckupVC = "introVctoCheckupsegue"
+    static let TableViewCellIdentifier = "cell"
+    static let UpcomingTableViewCellIdentifier = "UpcomingTableViewCell"
+}
